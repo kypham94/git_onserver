58fae3dd12ff46d094ce63826558e159eea51830
root <root@bittet.vps>
2018-11-05 22:43:51 -0500

    upload code

---
+++ backend/resources/views/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.master')
+@section('title', 'Không tìm thấy trang bạn yêu cầu')
+
+@section('content')
+	<div class="container"><h1>Không tìm thấy nội dung bạn yêu cầu</h1></div>
+@endsection
